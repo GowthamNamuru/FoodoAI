290238fe332c6c3af1f46fc0192e729ccbcea80b
gowthamnamuru <gowthamnamuri@gmail.com>
2026-01-30 20:33:46 +0530

    Initial Commit

---
+++ FoodoAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodoAI
+//
+//  Created by Gowtham Namuru on 30/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodoAI/FoodoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodoAIApp.swift
+//  FoodoAI
+//
+//  Created by Gowtham Namuru on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
